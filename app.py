f4bb00af2550bbb20eea27dd2029cb1a490d1619
Edson <egoncalves@tecgraf.puc-rio.br>
2022-07-29 23:02:21 +0000

    intervalo da aula6

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
 
@@ -54,7 +54,6 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-
         try:
             new_user = User(username=username, email=email)
             new_user.set_password(password)
@@ -66,24 +65,37 @@ def register():
             return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        user = User.query_filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
         if user is None or not user.check_password(password):
             flash("Incorrect Username or Password")
             return redirect(url_for('login'))
         login_user(user)
         return redirect(url_for('index'))
-
-
     return render_template("login.html")
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return render_template("index")
+    return redirect(url_for('login'))
+
+@app.route('/create', methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        try: 
+            post = Post(title=title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            flash("Error on create Post")
+    return render_template('create.html')
